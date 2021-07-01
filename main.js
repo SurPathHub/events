681ad845a21e7d2fb637d3974dba6dd9cfd4974c
vinzvinci <vincent.villafuerte1@gmail.com>
2021-07-01 21:12:33 +0800

    change time

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Aug 01, 2021 12:00:00").getTime();
+let launchDate = new Date("Aug 01, 2021 13:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
