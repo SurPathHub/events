658649bc88c46f681fbd4f88725f68d6bc287df8
vinzvinci <vincent.villafuerte1@gmail.com>
2021-07-02 12:49:05 +0800

    fix some duplicate css codes & added accordion faq feature

--- main.js
+++ main.js
@@ -37,3 +37,16 @@ function tick () {
     document.querySelector('.countdown').innerText = time;
   }
 }
+
+let li = document.querySelectorAll(".faq-text li");
+    for (var i = 0; i < li.length; i++) {
+      li[i].addEventListener("click", (e)=>{
+        let clickedLi;
+        if(e.target.classList.contains("question-arrow")){
+          clickedLi = e.target.parentElement;
+        }else{
+          clickedLi = e.target.parentElement.parentElement;
+        }
+       clickedLi.classList.toggle("showAnswer");
+      });
+    }
