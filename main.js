e29ff1726756f1405585f8abce0225f13e7b794a
vinzvinci <vincent.villafuerte1@gmail.com>
2021-07-06 17:43:29 +0800

    feat: redesign accordion & added more details

--- main.js
+++ main.js
@@ -38,15 +38,30 @@ function tick () {
   }
 }
 
-let li = document.querySelectorAll(".faq-text li");
-    for (var i = 0; i < li.length; i++) {
-      li[i].addEventListener("click", (e)=>{
-        let clickedLi;
-        if(e.target.classList.contains("question-arrow")){
-          clickedLi = e.target.parentElement;
-        }else{
-          clickedLi = e.target.parentElement.parentElement;
-        }
-       clickedLi.classList.toggle("showAnswer");
+    //Accordion
+    document.addEventListener("DOMContentLoaded", function(event) { 
+
+
+      var acc = document.getElementsByClassName("accordion");
+      var panel = document.getElementsByClassName('panel');
+      
+      for (var i = 0; i < acc.length; i++) {
+          acc[i].onclick = function() {
+              var setClasses = !this.classList.contains('active');
+              setClass(acc, 'active', 'remove');
+              setClass(panel, 'show', 'remove');
+      
+              if (setClasses) {
+                  this.classList.toggle("active");
+                  this.nextElementSibling.classList.toggle("show");
+              }
+          }
+      }
+      
+      function setClass(els, className, fnName) {
+          for (var i = 0; i < els.length; i++) {
+              els[i].classList[fnName](className);
+          }
+      }
+      
       });
-    }
